4cb5e47173d53b6aa4421f4e3b028479d27917f7
PalindromicBreadLoaf <plooff@tutanota.com>
2024-06-26 16:56:46 +0200

    Added suppor for loading in a proper file and have it be passed to the 'dissasembler'

--- main.cpp
+++ main.cpp
@@ -1,19 +1,55 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
-int main(int argc, char* argv[]) {
+int dissasemble8080(unsigned char *codebuffer, int pc) {
+    unsigned char *code = &codebuffer[pc];
+    int opbytes = 1;
+    std::cout << "%04x " << pc << std::endl;
+
+    switch (*code)
+    {
+
+    }
+
+    return opbytes;
+}
+
+int main(int argc, char* argv[])
+{
+    unsigned char *codebuffer;
+    int pc = 0;
+
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " filename" << std::endl;
         return 1;
     }
 
     std::string filename = argv[1];
+    std::ifstream file(filename, std::ios::in | std::ios::binary | std::ios::ate);
 
-    std::ifstream file(filename);
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         std::cerr << "Could not open file " << filename << std::endl;
         return 1;
+    }
+
+    std::streampos fsize = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    codebuffer = new unsigned char[fsize];
+
+    if (!file.read(reinterpret_cast<char*>(codebuffer), fsize))
+    {
+        std::cerr << "Error: Couldn't read the file " << filename << std::endl;
+        delete[] codebuffer;
+        return 1;
+    }
+
+    while (!file.eof())
+    {
+        pc = pc + dissasemble8080(codebuffer, pc);
     }
 
     file.close();
